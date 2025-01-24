38b3a720c3bbf79f67e6175c2d5137569ca0618c
Rohit3124 <rohitkumawat4493@gmail.com>
2025-01-25 01:49:07 +0530

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost/online-test")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
