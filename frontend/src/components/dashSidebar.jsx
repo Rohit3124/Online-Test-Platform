c86d8f7c49310ca23dc89e7ebffcdf26b6c0cd7d
Rohit3124 <rohitkumawat4493@gmail.com>
2025-01-26 02:21:12 +0530

    complete sign out functionality

--- frontend/src/components/dashSidebar.jsx
+++ frontend/src/components/dashSidebar.jsx
@@ -1,8 +1,9 @@
 import { Sidebar } from "flowbite-react";
 import { HiArrowSmRight, HiOutlineUserGroup } from "react-icons/hi";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const DashSidebar = () => {
+  const navigate = useNavigate();
   const [tab, setTab] = useState("");
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -11,6 +12,19 @@ const DashSidebar = () => {
       setTab(tabFromUrl);
     }
   }, [location.search]);
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (res.ok) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <Sidebar className="w-full md:w-56 ">
       <Sidebar.Items>
@@ -25,7 +39,11 @@ const DashSidebar = () => {
             </Sidebar.Item>
           </Link>
 
-          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+            onClick={handleSignout}
+          >
             Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
